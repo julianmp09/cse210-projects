fa2d7a8be8476053603d3e9c0b432abe0b7eb4a7
Julian Domselaar <juliandaniel9809@gmail.com>
2024-10-31 17:42:56 -0300

    add assigment one and two

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name?");
+        string question1 = Console.ReadLine();
+
+        Console.Write("What is your last name?");
+        string question2 = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {question2}, {question1} {question2}");
     }
 }
